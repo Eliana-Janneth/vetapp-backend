7b2e01abf260b4c2bed40cb1f0a37921e640239d
Juan Fernando Lopera Muñoz <100541364+JL005@users.noreply.github.com>
2023-09-25 15:07:37 -0500

    feat: GET method for search animal

--- animals/urls.py
+++ animals/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('animal-species/', views.AnimalSpeciesList.as_view(), name='animal-species-list'),
     path('animal-races/', views.AnimalRaceList.as_view(), name='animal-race-list'),
     path('animals/', views.AnimalList.as_view(), name='animal-list'),
+    path('animals/<int:pk>/', views.AnimalDetail.as_view(), name='animal-detail'),
 ]
