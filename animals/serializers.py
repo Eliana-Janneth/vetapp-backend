198fbc642853cffd1aed2dceec7780f9654f5f5c
Juan Fernando Lopera Muñoz <100541364+JL005@users.noreply.github.com>
2023-10-03 13:54:51 -0500

    fix: animal creation validation fixed

--- animals/serializers.py
+++ animals/serializers.py
@@ -21,36 +21,36 @@ class AnimalSerializer(serializers.ModelSerializer):
     race = serializers.PrimaryKeyRelatedField(
         required=True, queryset=Animal_Race.objects.all(), write_only=True,  error_messages={
             'required': 'La raza del animal es requerida',
-            'does_not_exist': 'La especie especificada no existe',
-            'incorrect_type': 'El nombre de la especie debe ser un número entero',
+            'does_not_exist': 'La raza especificada no existe',
+            'incorrect_type': 'El nombre de la raza debe ser un número entero',
         })
     specie_name = serializers.SerializerMethodField()
     race_name = serializers.SerializerMethodField()
     name = serializers.CharField(required=True, max_length=50, error_messages={
         'required': 'El nombre del animal es requerido',
         'max_length': 'El nombre del animal no puede tener más de 50 caracteres'
-    })
+        })
     color = serializers.CharField(required=True, max_length=50, error_messages={
         'required': 'El color del animal es requerido',
         'max_length': 'El color del animal no puede tener más de 50 caracteres'
-    })
+        })
     birth_date = serializers.DateField(required=True, error_messages={
         'required': 'La fecha de nacimiento del animal es requerida',
         'invalid': 'La fecha de nacimiento del animal debe tener el formato YYYY-MM-DD'
-    })
+        })
     gender = serializers.CharField(required=True, max_length=10, error_messages={
         'required': 'El género del animal es requerido',
         'max_length': 'El género del animal no puede tener más de 10 caracteres'
-    })
+        })
     weight = serializers.CharField(required=False, max_length=10, error_messages={
         'max_length': 'El peso del animal no puede tener más de 10 caracteres'
-    })
+        })
     height = serializers.CharField(required=False, max_length=10, error_messages={
         'max_length': 'La altura del animal no puede tener más de 10 caracteres'
-    })
+        })
     description = serializers.CharField(required=False, max_length=1000, error_messages={
         'max_length': 'La descripción del animal no puede tener más de 1000 caracteres'
-    })
+        })
 
     class Meta:
         model = Animals
