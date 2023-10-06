85f0ca67f7d34b9c66e63063e29d913b0c94b389
Juan Fernando Lopera Muñoz <100541364+JL005@users.noreply.github.com>
2023-10-06 15:03:23 -0500

    fix: fix API endpoint URL

--- animals/urls.py
+++ animals/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from animals import views
-from animals.views.animals_view import AnimalFarmer, AnimalDetail
+from animals.views.animals_view import AnimalsFarmer, AnimalDetail
 from animals.views.animal_species_view import AnimalSpeciesList
 from animals.views.animal_races_view import AnimalRaceList, AnimalRaceBySpecie
 
 urlpatterns = [
-    path('animals/', AnimalFarmer.as_view(), name='animal-list'),
+    path('animals/', AnimalsFarmer.as_view(), name='animal-list'),
     path('animals/<int:pk>/', AnimalDetail.as_view(), name='animal-detail'),
     path('animal-species/', AnimalSpeciesList.as_view(), name='animal-species-list'),
     path('animal-races/', AnimalRaceList.as_view(), name='animal-race-list'),
--- vets_authorization/views.py
+++ vets_authorization/views.py
@@ -1,35 +1,16 @@
+from helpers.views.auth_vet_view import AuthVetMixin
 from rest_framework.response import Response
 from rest_framework import status
 from animals.models import Animals
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
-from knox.auth import TokenAuthentication
-from knox.models import AuthToken
-from knox.settings import CONSTANTS
 from vets_authorization.models import Authorization
-from animals.serializers import AnimalSerializer
+from animals.serializers.animals import AnimalSerializer
 
-# Create your views here.
-def get_veterinarian(token):
-        user = AuthToken.objects.get(token_key=token[:CONSTANTS.TOKEN_KEY_LENGTH])
-        if not user or not user.user.role == 'veterinarian':
-            return None
-        return user.user
-
-def check_authentication(request):
-        if not request.headers['Authorization']:
-            return Response({'response': 'No estás logueado'}, status=status.HTTP_400_BAD_REQUEST)
-        token  = request.headers['Authorization'][6:]
-        return get_veterinarian(token)
-
-class AuthorizedAnimals(APIView):
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+class AuthorizedAnimals(AuthVetMixin, APIView):
 
     def get(self, request):
-        veterinarian = check_authentication(request) 
+        veterinarian = self.check_authentication(request) 
         list_authorized_animals = Authorization.objects.filter(veterinarian=veterinarian).values_list('animal', flat=True)
         authorized_animals = Animals.objects.filter(id__in=list_authorized_animals)
         serializer = AnimalSerializer(authorized_animals, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)        
-    pass
+        return Response(serializer.data, status=status.HTTP_200_OK)
