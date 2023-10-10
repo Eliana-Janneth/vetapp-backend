1776f0d5c961cc0060046166450407f6ee88ff2a
Juan Fernando Lopera Muñoz <100541364+JL005@users.noreply.github.com>
2023-10-09 19:21:06 -0500

    fix: endpoints fixed

--- medical_history/urls.py
+++ medical_history/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from medical_history.views.animal_medical_history_view import VetMedicalHistory, FarmerMedicalHistory
+from medical_history.views.vet_get_animal_info import AnimalInfoVet
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = format_suffix_patterns([
-    path('vet-medical-historys/<int:animal_id>', VetMedicalHistory.as_view()),
-    path('farmer-medical-historys/<int:animal_id>', FarmerMedicalHistory.as_view()),
+    path('vet-medical-historys/<int:animal_id>/', VetMedicalHistory.as_view()),
+    path('farmer-medical-historys/<int:animal_id>/', FarmerMedicalHistory.as_view()),
+    path('vet-animal-info/<int:animal_id>/', AnimalInfoVet.as_view()),
 ])
