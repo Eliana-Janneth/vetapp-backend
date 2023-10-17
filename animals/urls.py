163f37018d8a1bb042d3533c7ed8d804d0e8bffd
Juan Fernando Lopera Muñoz <100541364+JL005@users.noreply.github.com>
2023-10-16 21:39:52 -0500

    fix: endpoint url to animal list

--- animals/urls.py
+++ animals/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from animals import views
-from animals.views.animals_view import AnimalsFarmer, AnimalDetail, AnimalSearchByName, AnimalListSerializer
+from animals.views.animals_view import AnimalsFarmer, AnimalDetail, AnimalSearchByName, AnimalList
 from animals.views.animal_species_view import AnimalSpeciesList
 from animals.views.animal_races_view import AnimalRaceList, AnimalRaceBySpecie
 
 urlpatterns = [
     path('animals/', AnimalsFarmer.as_view(), name='animals'),
-    path('animals-list/', AnimalsFarmer.as_view(), name='animals-list'),
+    path('animals-list/', AnimalList.as_view(), name='animals-list'),
     path('animals/<int:pk>/', AnimalDetail.as_view(), name='animal-detail'),
     path('animals/search/', AnimalSearchByName.as_view(), name='animal-search-by-name'),
     path('animal-species/', AnimalSpeciesList.as_view(), name='animal-species-list'),
