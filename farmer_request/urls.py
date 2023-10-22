3f5db8d5a811c2e81d731c4e73b55e5bf5e9263c
Juan Fernando Lopera Muñoz <100541364+JL005@users.noreply.github.com>
2023-10-22 14:20:18 -0500

    feat: accept or reject a farmer request as vet

--- farmer_request/urls.py
+++ farmer_request/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from farmer_request.views.authorization_view import AuthorizedAnimals
 from farmer_request.views.create_vet_auth_view import Authorization
 from farmer_request.views.farmer_request_view import FarmerRequestView 
-from farmer_request.views.vet_request_view import PendingRequestAsVet
+from farmer_request.views.vet_request_view import PendingRequestAsVet, VetRequestView
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = format_suffix_patterns([
     path('authorized-animals/', AuthorizedAnimals.as_view()),
     path('authorization/', Authorization.as_view()),
     path('request/', FarmerRequestView.as_view(), name='request'),
-    path('vet-requests/', PendingRequestAsVet.as_view(), name='vet-request')
+    path('vet-requests/', PendingRequestAsVet.as_view(), name='vet-request'),
+    path('vet-requests/<int:farmer_request_id>/', VetRequestView.as_view(), name='vet-request-response'),
 ])
--- farmer_request/views/vet_request_view.py
+++ farmer_request/views/vet_request_view.py
@@ -1,19 +1,44 @@
 from helpers.views.auth_vet_view import AuthVetMixin
 from rest_framework.response import Response
-
+from animals.models import Animals
+from users.models import Veterinarian
 from rest_framework.views import APIView
 from farmer_request.models import FarmerRequest, Authorization
+from rest_framework import status
 
 from farmer_request.serializers.farmer_request import FarmerRequestAsVetSerializer
 
 class VetRequestView(AuthVetMixin, APIView):
     
-    def post(self, request):
-        pass    
+    def post(self, request, farmer_request_id):
+        vet = self.check_authentication(request)
+        if not vet:
+            return self.handle_error_response()
+        try:
+            farmer_request = FarmerRequest.objects.get(id=farmer_request_id)
+            vet_response = request.data.get('vet_response', False)
+            if vet_response is True:
+                farmer_request.status = 1
+                self.create_authorization(vet.id, farmer_request.animal.id)
+            else:
+                farmer_request.status = 2
+            farmer_request.save()
+            return Response(status=status.HTTP_200_OK)                
+        except FarmerRequest.DoesNotExist:
+            return self.handle_error_response()
 
 
-    def create_authorization(self, request, vet_id, animal_id):
-        authorization = Authorization()
+    def create_authorization(self, vet_id, animal_id):
+        try: 
+            animal = Animals.objects.get(id=animal_id)
+            vet = Veterinarian.objects.get(id=vet_id)
+            authorization = Authorization()
+            authorization.animal = animal
+            authorization.veterinarian = vet
+            authorization.save()
+        except (Animals.DoesNotExist, Veterinarian.DoesNotExist):
+            return self.handle_error_response()
+
 
 class PendingRequestAsVet(AuthVetMixin,APIView):
     def get(self, request):
