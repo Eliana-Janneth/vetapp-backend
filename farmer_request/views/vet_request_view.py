80e16b59d2d5cbb49748e3a72fbfcd77d49ec85b
Juan Fernando Lopera Muñoz <100541364+JL005@users.noreply.github.com>
2023-10-23 19:25:13 -0500

    fix: there can only be one authorization linking the animal to a veterinarian

--- farmer_request/views/vet_request_view.py
+++ farmer_request/views/vet_request_view.py
@@ -1,3 +1,4 @@
+from farmer_request.serializers.farmer_request import FarmerRequestAsVetSerializer
 from helpers.views.auth_vet_view import AuthVetMixin
 from rest_framework.response import Response
 from animals.models import Animals
@@ -6,10 +7,15 @@ from rest_framework.views import APIView
 from farmer_request.models import FarmerRequest, Authorization
 from rest_framework import status
 
-from farmer_request.serializers.farmer_request import FarmerRequestAsVetSerializer
+import resend
+from django.core.mail import EmailMessage, get_connection
+from django.conf import settings
+from django.http import JsonResponse
+
+
 
 class VetRequestView(AuthVetMixin, APIView):
-    
+
     def post(self, request, farmer_request_id):
         vet = self.check_authentication(request)
         if not vet:
@@ -19,28 +25,28 @@ class VetRequestView(AuthVetMixin, APIView):
             vet_response = request.data.get('vet_response', False)
             if vet_response is True:
                 farmer_request.status = 1
-                self.create_authorization(vet.id, farmer_request.animal.id)
+                create_response = self.create_authorization(vet.id, farmer_request.animal.id)
             else:
                 farmer_request.status = 2
             farmer_request.save()
-            return Response(status=status.HTTP_200_OK)                
+            return create_response
         except FarmerRequest.DoesNotExist:
             return self.handle_error_response()
 
-
     def create_authorization(self, vet_id, animal_id):
-        try: 
+        try:
             animal = Animals.objects.get(id=animal_id)
             vet = Veterinarian.objects.get(id=vet_id)
-            authorization = Authorization()
-            authorization.animal = animal
-            authorization.veterinarian = vet
+            if Authorization.objects.filter(animal=animal, veterinarian=vet).first():
+                return Response({'response': 'Ya existe una autorización para este veterinario y animal'},status=status.HTTP_400_BAD_REQUEST)
+            authorization = Authorization(animal = animal, veterinarian = vet)
             authorization.save()
+            return Response(status=status.HTTP_200_OK)
         except (Animals.DoesNotExist, Veterinarian.DoesNotExist):
             return self.handle_error_response()
 
 
-class PendingRequestAsVet(AuthVetMixin,APIView):
+class PendingRequestAsVet(AuthVetMixin, APIView):
     def get(self, request):
         vet = self.check_authentication(request)
         if not vet:
