7f035e97eb6fd94a34af8f9f0c628dc6bcf16cb7
Juan Fernando Lopera Muñoz <100541364+JL005@users.noreply.github.com>
2023-10-29 14:40:21 -0500

    fix: added animal specie to chat serializer

--- chatting/serializers/chat.py
+++ chatting/serializers/chat.py
@@ -12,12 +12,16 @@ class FarmerChatListSerializer(serializers.ModelSerializer):
 
         vet_name = serializers.SerializerMethodField()
         animal_name = serializers.SerializerMethodField()
+        animal_specie = serializers.SerializerMethodField()
 
         def get_vet_name(self, obj):
             return f"{obj.veterinarian.first_name} {obj.veterinarian.last_name}" if obj.veterinarian else None
         
         def get_animal_name(self, obj):
             return f"{obj.animal.name}" if obj.animal else None
+        
+        def get_animal_specie(self, obj):
+            return f"{obj.animal.specie}" if obj.animal else None
 
         class Meta:
             model = Chat
@@ -29,12 +33,17 @@ class VetChatListSerializer(serializers.ModelSerializer):
 
         farmer_name = serializers.SerializerMethodField()
         animal_name = serializers.SerializerMethodField()
+        animal_specie = serializers.SerializerMethodField()
+        
 
         def get_farmer_name(self, obj):
             return f"{obj.farmer.first_name} {obj.farmer.last_name}" if obj.farmer else None
         
         def get_animal_name(self, obj):
             return f"{obj.animal.name}" if obj.animal else None
+        
+        def get_animal_specie(self, obj):
+            return f"{obj.animal.specie}" if obj.animal else None
 
         class Meta:
             model = Chat
