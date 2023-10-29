ec1352a5bbeb6364e5650f14847c2ece7f1a091a
Juan Fernando Lopera Muñoz <100541364+JL005@users.noreply.github.com>
2023-10-28 20:40:35 -0500

    fix: message serializer for chat

---
+++ chatting/serializers/message.py
@@ -0,0 +1,10 @@
+from chatting.models import Message
+from rest_framework import serializers
+
+class MessageSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Message
+        exclude = ('chat',) 
+
+    
