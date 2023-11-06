b6cb7fe125056f7abbbaec7ba4c7884e7ca765b6
Juan Fernando Lopera Muñoz <100541364+JL005@users.noreply.github.com>
2023-11-05 19:04:19 -0500

    fix: get animal by name, specie or race as farmer

--- animals/views/animals_view.py
+++ animals/views/animals_view.py
@@ -2,8 +2,9 @@ from helpers.views.auth_farmer_view import AuthFarmerMixin
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from animals.models import Animals
+from animals.models import Animals, AnimalRaces, AnimalSpecies
 from animals.serializers.animals import AnimalSerializer, AnimalListSerializer, AnimalUpdateSerializer
+from django.db.models import Q
 
 class AnimalsFarmer(AuthFarmerMixin, APIView):
 
@@ -64,10 +65,12 @@ class AnimalSearchByName(AuthFarmerMixin, APIView):
         try:
             if not farmer:
                 return self.handle_error_response()
-            name = request.query_params.get('name', '')
-            if name == '':
+            search_query = request.query_params.get('by', '')
+            if search_query == '':
                 return Response({'response': 'El nombre del animal es requerido'}, status=status.HTTP_400_BAD_REQUEST)
-            animals = Animals.objects.filter(name__icontains=name, farmer=farmer.id)
+            animals = Animals.objects.filter(farmer=farmer.id)
+            animals = animals.filter(Q(name__icontains=search_query)|Q(race__in=AnimalRaces.objects.filter(Q(name__icontains=search_query))) |
+                                             Q(specie__in=AnimalSpecies.objects.filter(Q(name__icontains=search_query))))
             serializer = AnimalSerializer(animals, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Animals.DoesNotExist:
